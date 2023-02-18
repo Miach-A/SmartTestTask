9ee53a7ab207ed262151d9f7207798e0e545ae6a
Miach-A <ovmiach@gmail.com>
2023-02-19 01:08:06 +0200

    Swagger

--- SmartTestTask/Program.cs
+++ SmartTestTask/Program.cs
@@ -98,9 +98,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthentication();
 app.UseAuthorization();
